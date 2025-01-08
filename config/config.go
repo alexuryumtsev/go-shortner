4a73dc2f92d2f19d2ce3d363d74a527c96beb5a7
Alexey Uryumtsev <alexey.uryumtsev@softline.com>
2025-01-09 00:05:57 +0700

    iter5

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/url"
+	"os"
 	"regexp"
 )
 
@@ -15,28 +16,42 @@ type Config struct {
 func InitConfig() (*Config, error) {
 	cfg := &Config{}
 
+	// Получаем значения из переменных окружения.
+	envServerAddress := os.Getenv("SERVER_ADDRESS")
+	envBaseURL := os.Getenv("BASE_URL")
+
 	// Определяем флаги
-	flag.StringVar(&cfg.ServerAddress, "a", ":8080", "HTTP server address, host:port")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "Base URL for shortened links")
+	flag.StringVar(&cfg.ServerAddress, "a", "", "HTTP server address, host:port")
+	flag.StringVar(&cfg.BaseURL, "b", "", "Base URL for shortened links")
 
 	// Обрабатываем флаги
 	flag.Parse()
 
-	// Проверяем значения флагов
-	if cfg.ServerAddress != "" {
-		// Проверка формата host:port
-		hostPortPattern := `^([a-zA-Z0-9.-]+)?(:[0-9]+)$`
-		matched, err := regexp.MatchString(hostPortPattern, cfg.ServerAddress)
-		if err != nil || !matched {
-			return nil, fmt.Errorf("invalid server address format, expected host:port")
-		}
+	// Проверяем значения флагов и переменных окружения
+	if cfg.ServerAddress == "" {
+		cfg.ServerAddress = envServerAddress
+	}
+	if cfg.ServerAddress == "" {
+		cfg.ServerAddress = ":8080" // Значение по умолчанию.
 	}
 
-	if cfg.BaseURL != "" {
-		_, err := url.ParseRequestURI(cfg.BaseURL)
-		if err != nil {
-			return nil, fmt.Errorf("invalid base URL: %v", err)
-		}
+	// Проверка формата host:port
+	hostPortPattern := `^([a-zA-Z0-9.-]+)?(:[0-9]+)$`
+	matched, err := regexp.MatchString(hostPortPattern, cfg.ServerAddress)
+	if err != nil || !matched {
+		return nil, fmt.Errorf("invalid server address format, expected host:port")
+	}
+
+	if cfg.BaseURL == "" {
+		cfg.BaseURL = envBaseURL
+	}
+	if cfg.BaseURL == "" {
+		cfg.BaseURL = "http://localhost:8080/" // Значение по умолчанию.
+	}
+
+	_, err = url.ParseRequestURI(cfg.BaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("invalid base URL: %v", err)
 	}
 
 	return cfg, nil
